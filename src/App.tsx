df75535131f295f4d6e53f89df8357081140baeb
Keiko Chuang <keiko@innotech.me>
2021-01-18 17:07:01 +0800

    20210118-edit-title

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          React with Typescript Support
+          React with Typescript & Docker Support
         </p>
         <a
           className="App-link"
