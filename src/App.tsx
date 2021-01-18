7e173b81e0615c69f2ada5e142db6339b44f8a8e
Keiko Chuang <keiko@innotech.me>
2021-01-18 16:10:27 +0800

    20210118-project-setup-react-core-with-ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/img/logo.svg';
 import './App.css';
 
 function App() {
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          React with Typescript Support
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/keiko15678/react-ts-template"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Github
         </a>
       </header>
     </div>
